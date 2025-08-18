20d8d32e664be60fae4c0dfe320a54c4cc4ff2d5
leao <lucasleaolima@gmail.com>
2025-08-17 23:47:20 -0300

    feat: ci/cd tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,8 +5,5 @@ export default defineConfig({
         baseUrl: 'http://localhost:5173',
         supportFile: 'cypress/support/e2e.ts',
         specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        }
     }
 })
